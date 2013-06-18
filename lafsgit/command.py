4073889981d4bf4a1f6567770a74fef89ce6858a
Nathan Wilcox <nejucomo@gmail.com>
2013-06-18 07:46:26 -0700

    Some more exploratory design of git command handling.

--- lafsgit/command.py
+++ lafsgit/command.py
@@ -1,10 +1,13 @@
 import os
+from twisted.internet import defer
 from twisted.protocols import basic
 
 from .logmixin import LogMixin
 
 
 class CommandProtocol (basic.LineReceiver, LogMixin):
+
+    GitCapabilities = ['option']
 
     delimiter = os.linesep
 
@@ -15,7 +18,24 @@ class CommandProtocol (basic.LineReceiver, LogMixin):
     def connectionMade(self):
         self.log.debug('Ready for commands.')
 
-    def lineReceived(self, line):
-        self.log.debug('Received %r', line)
+    #def dataReceived(self, data): # Just for temporary tracing debug.
+    #    self.log.debug('dataReceived(%r)', data)
+    #    return basic.LineReceiver.dataReceived(self, data)
 
-        from twisted.internet import reactor; reactor.stop(); raise NotImplementedError('lineReceived')
+    def lineReceived(self, line):
+        self.log.debug('Received line %r', line)
+
+        args = line.split(' ', 1)
+        name = args.pop(0)
+
+        cmdfunc = getattr(self, 'command_' + name)
+
+        d = defer.maybeDeferred(cmdfunc, *args)
+
+        @d.addCallback
+        def handle_response(response):
+            self.log.debug('Sending response for %r:\n%s', name, response)
+            self.transport.write(response + self.delimiter)
+
+    def command_capabilities(self):
+        return self.delimiter.join(self.GitCapabilities) + self.delimiter
