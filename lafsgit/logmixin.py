7f10f7ef03605b358955eeeb6f37041a77822e97
Nathan Wilcox <nejucomo@gmail.com>
2014-08-01 13:36:08 -0700

    Remove everything in order to start a go-based implementation.

--- lafsgit/logmixin.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-
-class LogMixin (object):
-    def __init__(self):
-        self._log = logging.getLogger(self.__class__.__name__)
--- lafsgit/notimplemented.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-
-
-def not_implemented(tmpl, *args):
-    sys.stderr.write('Not Implemented: %s\n' % (tmpl % args,))
-    os._exit(-7)
