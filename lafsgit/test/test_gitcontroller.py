782ae95a336818e71d158486fcc08c2ccbbe409d
Nathan Wilcox <nejucomo@gmail.com>
2014-08-15 16:35:20 -0700

    Start specifying how GitController responds to commands.

--- lafsgit/test/test_gitcontroller.py
+++ lafsgit/test/test_gitcontroller.py
@@ -1,16 +1,38 @@
 from twisted.trial.unittest import TestCase
 
-from mock import call, patch, sentinel
+from mock import MagicMock, call, patch
 
 from lafsgit.gitcontroller import GitController
 
 
 class GitControllerTests (TestCase):
 
-    @patch('logging.getLogger')
-    def test__init__calls_getLogger(self, m_getLogger):
+    def setUp(self):
+        self.m_lafsclient = MagicMock()
 
-        GitController(sentinel.lafsclient)
+        with patch('logging.getLogger') as m_getLogger:
+            self.m_getLogger = m_getLogger
+            self.gc = GitController(self.m_lafsclient)
 
+
+    def test__init__calls_getLogger(self):
         # Note: This asserts there are no log calls in the constructor:
-        self.assertEqual(m_getLogger.mock_calls, [call('GitController')])
+        self.assertEqual(self.m_getLogger.mock_calls, [call('GitController')])
+
+
+    def test_handle_git_command_capabilities(self):
+        self.assertEqual(['push'], self._hgcs('capabilities'))
+
+
+    def _hgcs(self, *commandlines):
+        """handle_git_command(s)
+
+        This asserts that all but the last command returns None.
+        It returns the result of the last command.
+        """
+        lastresult = None
+        for cmd in commandlines:
+            result = self.gc.handle_git_command(cmd)
+            self.assertIsNone(lastresult)
+            lastresult = result
+        return lastresult
