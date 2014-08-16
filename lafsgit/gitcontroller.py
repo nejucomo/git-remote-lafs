be40717928e8602340ea41aa7bc3cc936e904fc1
Nathan Wilcox <nejucomo@gmail.com>
2014-08-15 17:10:24 -0700

    Implement the capabilities git command specification.

--- lafsgit/gitcontroller.py
+++ lafsgit/gitcontroller.py
@@ -4,3 +4,9 @@ from lafsgit.logmixin import LogMixin
 class GitController (LogMixin):
     def __init__(self, lafsclient):
         LogMixin.__init__(self)
+
+    def handle_git_command(self, line):
+        if line == 'capabilities':
+            return ['push']
+        else:
+            raise NotImplementedError('Unsupported Command')
